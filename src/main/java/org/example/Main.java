771b0a35b78756d7305f6f9d443804470027620d
ilaydapasli <ilayda.pasli2018@stu.pirireis.edu.tr>
2025-01-06 18:27:33 +0300

    .

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,9 +2,10 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-        System.out.println(shouldWakeUp(true, 9));
-        System.out.println (area(-1.0, 4.0));// true
+        System.out.println(isCatPlaying(true, 10));
+        System.out.println( isCatPlaying(false, 36));
+        System.out.println(isCatPlaying(false, 35));
+
     }
 
     public static boolean shouldWakeUp(boolean isBarking, int clock) {
@@ -25,9 +26,11 @@ public class Main {
         }return false;
 
     }  public static boolean isCatPlaying(boolean isSummer, int temp) {
-            if((temp>25 && temp<35)||!isSummer){
+            if((temp>25 && temp<=35)&&!isSummer){
                 return true;
-            }else {
+            } else if ((temp>25 && temp<45)&&isSummer) {
+                return true;
+            } else {
                 return false;
             }
 
