b8ad0b2c8860427f4b899159612c59ee61fa9b5e
ilaydapasli <ilayda.pasli2018@stu.pirireis.edu.tr>
2025-01-06 18:12:23 +0300

    last commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,7 +3,8 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        System.out.println(shouldWakeUp(true, 9));    // true
+        System.out.println(shouldWakeUp(true, 9));
+        System.out.println (area(-1.0, 4.0));// true
     }
 
     public static boolean shouldWakeUp(boolean isBarking, int clock) {
@@ -30,6 +31,21 @@ public class Main {
                 return false;
             }
 
+    }
+    public static double area(double width, double height) {
+        if(width<0||height<0){
+            return -1;
+        } else {
+            return width*height;
+        }
+    }
+
+    public static double area(double radius) {
+        if (radius<0){
+            return -1;
+        }else {
+            return radius*radius*Math.PI;
+        }
     }
 
 
